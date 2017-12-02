178774efffc732d8a171369748762f23e5405885
Joe Sycalik <sycalik@gmail.com>
2017-12-02 15:01:54 -0500

    Added GameManager

---
+++ LD40/Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public int playerCount;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    } //End Awake
+} //End class
+
