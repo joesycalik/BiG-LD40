ac9e60e7a2275d626106ceb9cff12cd1836555d4
tonyli99 <tony@pixelcrushers.com>
2017-12-02 16:45:44 -0500

    Start audio

---
+++ LD40/Assets/Scripts/GameSoundManager.cs
@@ -0,0 +1,48 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class GameSoundManager : MonoBehaviour {
+
+//    private static GameSoundManager m_instance = null;
+//    public static GameSoundManager instance
+//    {
+//        get
+//        {
+//            if (m_instance == null)
+//            {
+//                m_instance = Resources.Load<GameSoundManager>("GameSoundManager");
+//            }
+//            return m_instance;
+//        }
+//    }
+
+//    private AudioSource m_audioSource = null;
+//    public AudioSource audioSource
+//    {
+//        get
+//        {
+//            if (m_audioSource == null)
+//            {
+//                m_audioSource = gameObject.AddComponent<AudioSource>();
+//                m_audioSource.pan
+//            }
+//            return m_audioSource;
+//        }
+//    }
+
+//    public void PlayJump()
+//    {
+//        if
+//    }
+
+//    public void PlayGetGem()
+//    {
+
+//    }
+
+//    public void PlayHit()
+//    {
+
+//    }
+//}
